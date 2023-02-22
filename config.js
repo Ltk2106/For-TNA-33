2c268a9ec70bace30703fc8a5ba4d29c2ffd91e6
Ltk2106 <115920256+Ltk2106@users.noreply.github.com>
2023-02-22 22:00:07 +0700

    yep

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Ngọc Anh';
+const giftUrl = 'https://www.facebook.com/lili.ngocanh';
+const eventName = 'Sorry you:33';
+const titleCard = 'Dear Ngọc Anh';
+const contentCard = 'Anh sai ròi, anh xin lỗi em luônnnn( ánh mắt van nài sự tha thứ).';
+
+
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'dear.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
